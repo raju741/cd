1dfd574f0b33ab9ca5eec8ad6cd72cf077d07f15
raju741 <155701790+raju741@users.noreply.github.com>
2024-01-12 10:37:00 +0530

    Update Requre_demo.java

--- src/test/java/requre/Requre_demo.java
+++ src/test/java/requre/Requre_demo.java
@@ -17,7 +17,7 @@ public class Requre_demo {
 
 		driver.get("https://www.demoblaze.com/");
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 	}
 
 	@Test(priority = 1)
